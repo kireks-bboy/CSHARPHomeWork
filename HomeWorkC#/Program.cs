130112a1b4c464c4999c392b6a72851822430021
Kirill Denisevitch <denviko00@gmail.com>
2022-11-20 12:27:23 +0300

    homeworkC

---
+++ HomeWorkC#/Program.cs
@@ -0,0 +1,81 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// Console.Write("Enter the first number: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter the second number: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// if (a > b)
+// {
+//     Console.WriteLine($"Max number is: {a}");
+// }
+// else if (a < b)
+// {
+//     Console.WriteLine($"Max number is: {b}");
+// } else 
+// {
+//     Console.WriteLine("Numbers are equal");
+// }
+
+
+
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// Console.Write("Enter the 1st number: ");
+// int n1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter the 2nd number: ");
+// int n2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter the 3rd number: ");
+// int n3 = Convert.ToInt32(Console.ReadLine());
+
+// if (n1 > n2 && n1 > n3)
+// {
+//     Console.WriteLine($"Max number is: {n1}");
+// }
+// else if (n2 > n1 && n2 > n3)
+// {
+//     Console.WriteLine($"Max number is: {n2}");
+// } 
+// else if (n3 > n1 && n3 > n2)
+// {
+//     Console.WriteLine($"Max number is: {n3}");
+// }
+// else
+// {
+//     Console.WriteLine("Numbers are equal");
+// }
+
+
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// Console.Write("Please, enter the integer number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if (num % 2 == 0)
+// {
+//     Console.WriteLine($"{num} is even");
+// }
+// else 
+// {
+//     Console.WriteLine($"{num} is odd");
+// }
+
+
+
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// Console.Write("Enter the number: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// int count = 1;
+// while (n >= count)
+// {
+//     if (count % 2 == 0)
+//     {
+//         Console.Write(count + " ");
+//     }
+//     count++;
+// }
